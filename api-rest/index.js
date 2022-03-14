4bb2234ac208b83f3ceb0b64bcab3bca442f67c3
Pablo Aller <pablusprogramador@gmail.com>
2022-03-14 03:06:24 +0100

    continuacion apirest

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product')
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -23,8 +26,21 @@ app.get('api/product/:productId', (req,res) => {
 })
 
 app.post('/api/product', (req,res) => {
+    console.log('POST /api/product')
     console.log(req.body);
-    res.status(200).send({message: 'El producto se ha recibido'})
+
+    let product = new Product();
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+
+    product.save((err, productStored) => {
+        if (err) res.status(500).send({message: `Error al salvar en la BBDD: ${err}`})
+
+        res.status(200).send({product: productStored})
+    })
 })
 
 app.put('/api/product/:productId', (req,res) => {
@@ -35,6 +51,13 @@ app.delete('/api/product/:productId', (req,res) => {
 
 })
 
-app.listen(port, () => {
-    console.log(`API REST funcionando en http://localhost:${port}`)
+
+mongoose.connect('mongodb://localhost:27017/shop', (err ,res) => {
+    if (err) throw err;
+    console.log('Conexion a la bbdd establecida');
+
+    app.listen(port, () => {
+        console.log(`API REST funcionando en http://localhost:${port}`)
+    })
+
 })
