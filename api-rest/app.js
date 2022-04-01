8aafd24633c9ec682edbf946977bb2acdf4e021c
Pablo Aller <pablusprogramador@gmail.com>
2022-04-02 00:59:50 +0200

    video19

--- api-rest/app.js
+++ api-rest/app.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require( 'body-parser')
 const hbs = require('express-handlebars')
-const app = express();
+const app = express()
 const api = require('./routes')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.engine('.hbs', hbs({
-    defaultLayout: 'default',
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.engine('.hbs', hbs.engine({
+  defaultLayout: 'default',
+  extname: '.hbs'
+}))
 
-app.use('/api' , api)
+app.set('view engine', '.hbs')
+app.use('/api', api)
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+module.exports = app
 
 module.exports = app;
